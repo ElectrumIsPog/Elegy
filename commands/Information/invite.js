4608a488c7d417e9018c55a9dc72a73ac79b8240
FINE-V3rm <56572360+FINE-V3rm@users.noreply.github.com>
2021-06-25 11:49:21 -0400

    Create invite.js

---
+++ commands/Information/invite.js
@@ -0,0 +1,20 @@
+const config = require("../../botconfig/config.json"); //loading config file with token and prefix, and settings
+const ee = require("../../botconfig/embed.json"); //Loading all embed settings like color footertext and icon ...
+const Discord = require("discord.js"); //this is the official discord.js wrapper for the Discord Api, which we use!
+const {escapeRegex} = require("../../handlers/functions"); //Loading all needed functions
+module.exports = {
+    name: "invite",
+    category: "Information",
+    aliases: ["i", "join", "invitebot", "botinvite"],
+    cooldown: 4,
+    usage: "invite",
+    description: "Allows you to invite are amazing bot!",
+    run: async (client, message, args, user, text, prefix) => {
+    const embed = new Discord.MessageEmbed()
+        .setTitle(`Add the bot to your server!`)
+        .setColor(`GREEN`)
+        .addField(`Invite`, `https://discord.com/oauth2/authorize?client_id=857775470279000085&scope=bot&permissions=8`)
+        .setFooter(`Reqested by ${message.author.username}`)
+        message.channel.send(embed);
+    }
+}
