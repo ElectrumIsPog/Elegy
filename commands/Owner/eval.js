9e452b0cc23f3e7f22be3148754bad1c2af23599
FINE-V3rm <56572360+FINE-V3rm@users.noreply.github.com>
2021-06-24 21:26:29 -0400

    Push base code

---
+++ commands/Owner/eval.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: "eval",
+    category: "Owner",
+    aliases: ["ev"],
+    cooldown: 2,
+    usage: "eval <code>",
+    description: "Gives you information on how fast the Bot can respond to you",
+    
+}
