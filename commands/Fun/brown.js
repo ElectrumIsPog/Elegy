5749b7f39d84a87b75509aab152881d57a26bf7d
FINE-V3rm <56572360+FINE-V3rm@users.noreply.github.com>
2021-06-26 02:39:01 -0400

    add credits command

--- commands/Fun/brown.js
+++ commands/Fun/brown.js
@@ -12,7 +12,7 @@ module.exports = {
         let niggers = [`https://i.pinimg.com/originals/74/3d/ae/743dae5683f45ea6eae94e5ee30db253.jpg`,`https://i.pinimg.com/originals/ea/fa/a5/eafaa558e8a22e6b2e456fcc7a0b38d8.jpg`,`https://upload.wikimedia.org/wikipedia/commons/1/11/Faizon_Love_at_Porn_Star_Karaoke.jpg`,`https://i.ytimg.com/vi/vxMcjxpWUlg/maxresdefault.jpg`,`https://i.ytimg.com/vi/vxMcjxpWUlg/maxresdefault.jpg`]
         let randomized = niggers[Math.floor(Math.random() * niggers.length)]
         const niggerEmbed = new Discord.MessageEmbed()
-        .setTitle(`A dirty nigger!`)
+        .setTitle(`The nigger you requested, master.`)
         .setColor(`BROWN`)
         .setImage(`${randomized}`)
         .setFooter(`Requested by ${message.author.username}`)
---
+++ commands/Information/credits.js
@@ -0,0 +1,21 @@
+const config = require("../../botconfig/config.json"); //loading config file with token and prefix, and settings
+const ee = require("../../botconfig/embed.json"); //Loading all embed settings like color footertext and icon ...
+const Discord = require("discord.js"); //this is the official discord.js wrapper for the Discord Api, which we use!
+const {escapeRegex} = require("../../handlers/functions"); //Loading all needed functions
+
+module.exports = {
+    name: "credits",
+    category: "Infomation",
+    aliases: ["git"],
+    cooldown: 0,
+    usage: "credits",
+    run: async (client, message, args, user, text, prefix) => { 
+        let creditsEmbed = new Discord.MessageEmbed()
+        .setFooter(ee.footertext, ee.footericon)
+        .setTitle(`Credits list`)
+        .addField(`Main devloper`, `Electrum`)
+        .setColor(ee.color)
+        .addField(`Contributors`, `Nickk & Dobro bot\nTomato - Assisting in recoding help command\n Cow - Fixing dog command\n You! ${message.author.username} - for being a cool person!`)
+        message.channel.send(creditsEmbed)
+    }
+}
